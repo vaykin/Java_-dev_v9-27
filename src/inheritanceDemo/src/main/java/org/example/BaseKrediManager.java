b9517d5cd843a682e27518f66b04081440796d26
vaykin <162468086+vaykin@users.noreply.github.com>
2024-05-29 10:59:15 +0300

    Add files via upload

---
+++ src/inheritanceDemo/src/main/java/org/example/BaseKrediManager.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class BaseKrediManager {
+    public void Hesapla(){
+        System.out.println("Kredi hesaplandı");
+    }
+    }
+
---
+++ src/inheritanceDemo/src/main/java/org/example/KrediUI.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class KrediUI {
+    public void KrediHesapla(BaseKrediManager baseKrediManager){
+        baseKrediManager.Hesapla();
+
+    }
+}
